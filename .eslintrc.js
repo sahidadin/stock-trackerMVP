afac07167fa30065f746105e54f9ac21862f4c62
Sahid Adin <sahidadin2@gmail.com>
2023-06-17 16:10:16 -0700

    mvp

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "settings": {
+            "react": {
+              "version": 'detect',
+            },
+          },
+
+    }
+}
